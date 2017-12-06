f2a454169adaee4d2dd5630d28721feea821127b
Justin <justin-804@hotmail.com>
2017-12-06 12:13:31 +0100

    name change

--- src/main/java/io/dropwizard/ApiApplication.java
+++ src/main/java/io/dropwizard/ApiApplication.java
@@ -1,14 +1,13 @@
 package io.dropwizard;
 
-import io.dropwizard.Application;
 import io.dropwizard.resources.PersoneelResource;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class lalaApplication extends Application<lalaConfiguration> {
+public class ApiApplication extends Application<ApiConfiguration> {
 
     public static void main(final String[] args) throws Exception {
-        new lalaApplication().run(args);
+        new ApiApplication().run(args);
     }
 
     @Override
@@ -17,12 +16,12 @@ public class lalaApplication extends Application<lalaConfiguration> {
     }
 
     @Override
-    public void initialize(final Bootstrap<lalaConfiguration> bootstrap) {
+    public void initialize(final Bootstrap<ApiConfiguration> bootstrap) {
         // TODO: application initialization
     }
 
     @Override
-    public void run(final lalaConfiguration configuration,
+    public void run(final ApiConfiguration configuration,
                     final Environment environment) {
         final PersoneelResource personeel = new PersoneelResource();
         environment.jersey().register(personeel);
--- src/main/java/io/dropwizard/ApiConfiguration.java
+++ src/main/java/io/dropwizard/ApiConfiguration.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
-public class lalaConfiguration extends Configuration {
+public class ApiConfiguration extends Configuration {
     // TODO: implement service configuration
 }
