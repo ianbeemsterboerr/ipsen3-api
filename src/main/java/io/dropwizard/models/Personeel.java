39a3a4284d9ad4c3edf2d98592adfa051d74a894
Justin <justin-804@hotmail.com>
2017-12-07 13:47:03 +0100

    API WERKT!!!!!!!!!!!!

--- src/main/java/io/dropwizard/models/Personeel.java
+++ src/main/java/io/dropwizard/models/Personeel.java
@@ -13,6 +13,62 @@ public class Personeel implements Principal{
     @Length(min = 3, max = 40)
     private String voornaam;
 
+    public int getPersoneelID() {
+        return personeelID;
+    }
+
+    public void setPersoneelID(int personeelID) {
+        this.personeelID = personeelID;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
+
+    public String getRechten() {
+        return rechten;
+    }
+
+    public void setRechten(String rechten) {
+        this.rechten = rechten;
+    }
+
+    public String getWerkzaam() {
+        return werkzaam;
+    }
+
+    public void setWerkzaam(String werkzaam) {
+        this.werkzaam = werkzaam;
+    }
+
     private String tussenvoegsel;
 
     @NotEmpty
@@ -54,6 +110,7 @@ public class Personeel implements Principal{
         this.werkzaam = werkzaam;
 
     }
+
 
     @Override
     public String getName() {
