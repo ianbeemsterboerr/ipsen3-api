680f4bed0f7942da585e999aa7be96d62945c9e4
StalenStan <stanhoenson@icloud.com>
2018-01-17 15:37:15 +0100

    toch? ja.

--- src/main/java/io/dropwizard/services/AuthService.java
+++ src/main/java/io/dropwizard/services/AuthService.java
@@ -1,35 +1,38 @@
 package io.dropwizard.services;
 
-import com.google.common.base.Optional;
+import java.util.Optional;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.Authorizer;
 import io.dropwizard.auth.basic.BasicCredentials;
-import io.dropwizard.models.Personeel;
-import io.dropwizard.persistence.DAO.PersoneelDAO;
+import io.dropwizard.models.Employee;
+import io.dropwizard.persistence.DAO.EmployeeDAO;
 import org.mindrot.jbcrypt.BCrypt;
 
+import javax.inject.Inject;
 
-public class AuthService implements Authenticator<BasicCredentials, Personeel>, Authorizer<Personeel> {
-    private PersoneelDAO dao;
 
-    public AuthService(PersoneelDAO dao){
+public class AuthService implements Authenticator<BasicCredentials, Employee>, Authorizer<Employee> {
+    private EmployeeDAO dao;
+
+    @Inject
+    public AuthService(EmployeeDAO dao){
         this.dao = dao;
     }
 
     @Override
-    public Optional<Personeel> authenticate(BasicCredentials basicCredentials) throws AuthenticationException {
-        Personeel persoon = dao.getByEmailaddress(basicCredentials.getUsername());
-        if (persoon != null && persoon.getPassword() != null){
+    public Optional<Employee> authenticate(BasicCredentials basicCredentials) throws AuthenticationException {
+        Employee persoon = dao.getByEmailaddress(basicCredentials.getUsername());
+        if (persoon != null && persoon.getWachtwoord() != null){
             if(BCrypt.checkpw(basicCredentials.getPassword(), persoon.getWachtwoord())){
                 return Optional.of(persoon);
             }
         }
-        return Optional.absent();
+        return Optional.empty();
     }
 
     @Override
-    public boolean authorize(Personeel user, String roleName)
+    public boolean authorize(Employee user, String roleName)
     {
         return user.getRechten().equals(roleName);
     }
