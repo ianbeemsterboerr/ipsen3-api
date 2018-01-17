092dc4f97f91f0212e527bac77c18a6a788a9444
Justin <justin-804@hotmail.com>
2018-01-17 10:49:23 +0100

    Authorisatie

--- src/main/java/io/dropwizard/services/AuthService.java
+++ src/main/java/io/dropwizard/services/AuthService.java
@@ -3,12 +3,14 @@ package io.dropwizard.services;
 import com.google.common.base.Optional;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.Authorizer;
 import io.dropwizard.auth.basic.BasicCredentials;
 import io.dropwizard.models.Personeel;
 import io.dropwizard.persistence.DAO.PersoneelDAO;
 import org.mindrot.jbcrypt.BCrypt;
 
-public class AuthService implements Authenticator<BasicCredentials, Personeel> {
+
+public class AuthService implements Authenticator<BasicCredentials, Personeel>, Authorizer<Personeel> {
     private PersoneelDAO dao;
 
     public AuthService(PersoneelDAO dao){
@@ -24,5 +26,11 @@ public class AuthService implements Authenticator<BasicCredentials, Personeel> {
             }
         }
         return Optional.absent();
+    }
+
+    @Override
+    public boolean authorize(Personeel user, String roleName)
+    {
+        return user.getRechten().equals(roleName);
     }
 }
